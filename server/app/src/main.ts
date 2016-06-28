4c77c908aca4cb0d31ed46f8f181107ed2fb7919
Ondřej Kupka <ondra.cap@gmail.com>
2016-06-28 10:50:19 +0200

    webapp: Use new Angular 2 forms

--- server/app/src/main.ts
+++ server/app/src/main.ts
@@ -1,9 +1,11 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { bootstrap }                            from '@angular/platform-browser-dynamic';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { AppComponent }         from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 
 bootstrap(AppComponent, [
+  disableDeprecatedForms(), provideForms(),
   APP_ROUTER_PROVIDERS
 ]);
