91946bd4168e01bc1aa5a42e41c70fd1e89626e5
Ondřej Kupka <ondra.cap@gmail.com>
2016-07-27 18:55:44 +0200

    Event Stream: Add desktop notifications
    The event stream now uses desktop notifications when possible. The limit
    is set to receive max 1 notification a minute.
    
    Resolves #28

--- server/app/src/routes/eventstream/components/eventstream.component.ts
+++ server/app/src/routes/eventstream/components/eventstream.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/buffer';
+import 'rxjs/add/operator/do';
 
 import { ReconnectingWebSocket } from '../../../common/ReconnectingWebSocket';
 
@@ -15,6 +18,10 @@ import { EventModel }         from '../models/event.model';
 
 
 const MAX_FEED_SIZE = 10000;
+const DESKTOP_NOTIFICATIONS_INTERVAL = 5 * 60 * 1000; // 1 minute
+
+
+declare var Notification: any;
 
 
 @Component({
@@ -66,10 +73,77 @@ export class EventStreamComponent implements OnInit, OnDestroy {
     this.socket.errors
       .subscribe(err => console.error('WebSocket', err));
 
+    this.connectDesktopNotifications(this.socket.messages);
+
     this.socket.connect();
   }
 
   ngOnDestroy() {
     this.socket.close();
+  }
+
+  private connectDesktopNotifications(messages: Observable<MessageEvent>) : void {
+    // Make sure notifications are available.
+    if (!('Notification' in window)) {
+      this.messageService.warning('Desktop notifications are not supported in this browser');
+      return;
+    }
+
+    let notification;
+    const triggerNotification = (numEvents) => {
+      if (notification) {
+        notification.close();
+        notification = null;
+      }
+
+      notification = new Notification('SteemWatch Event Stream', {
+        icon: 'https://steemit.com/images/favicons/favicon-128.png',
+        body: `${numEvents} new ${numEvents === 1 ? 'event' : 'events'} received`,
+        tag:  'steemwatch'
+      });
+    };
+
+    // Start notifications if allowed.
+    let count = 0;
+    let freeToGo = true;
+    let dirty = false;
+
+    const onEvent = () => {
+      count++;
+
+      if (freeToGo) {
+        triggerNotification(count);
+        count = 0;
+
+        freeToGo = false;
+        dirty = false;
+
+        Observable.timer(DESKTOP_NOTIFICATIONS_INTERVAL)
+          .subscribe(() => {
+            freeToGo = true;
+            if (dirty) {
+              count--;
+              setTimeout(onEvent, 0);
+            }
+          });
+      } else {
+        dirty = true;
+      }
+    };
+
+    const startNotifications = () => messages.subscribe(onEvent);
+
+    if (Notification.permission === 'granted') {
+      startNotifications();
+      return;
+    }
+
+    if (Notification.permission !== 'denied') {
+      Notification.requestPermission(permission => {
+        if (permission === 'granted') {
+          startNotifications();
+        }
+      })
+    }
   }
 }
