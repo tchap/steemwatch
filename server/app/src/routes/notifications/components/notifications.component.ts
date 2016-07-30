f4dff21fe7ef96e82354c5f9b69075912899ada3
Ondřej Kupka <ondra.cap@gmail.com>
2016-07-30 13:17:18 +0200

    wip [ci skip]

--- server/app/src/routes/notifications/components/notifications.component.ts
+++ server/app/src/routes/notifications/components/notifications.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../../../services/index';
 
-import { SlackComponent }      from './slack.component';
-import { RocketChatComponent } from './rocket-chat.component';
+import { SlackComponent }       from './slack.component';
+import { SteemitChatComponent } from './steemit-chat.component';
 
 
 @Component({
   moduleId: module.id,
   templateUrl: 'notifications.component.html',
-  directives: [SlackComponent, RocketChatComponent]
+  directives: [SlackComponent, SteemitChatComponent]
 })
 export class NotificationsComponent implements OnInit {
 
---
+++ server/app/src/routes/notifications/components/steemit-chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'steemit-chat',
+  templateUrl: 'steemit-chat.component.html',
+  styleUrls: ['steemit-chat.component.css']
+})
+export class SteemitChatComponent {}
