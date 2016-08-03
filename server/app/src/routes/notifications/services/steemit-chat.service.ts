046c21dfeb32fceab3ddd97a8e9227029cdf3ba5
Ondřej Kupka <ondra.cap@gmail.com>
2016-08-03 16:36:25 +0200

    wip [ci skip]

--- server/app/src/routes/notifications/services/steemit-chat.service.ts
+++ server/app/src/routes/notifications/services/steemit-chat.service.ts
@@ -8,6 +8,15 @@ import { CookieService } from 'angular2-cookie/core';
 import { SteemitChatModel } from '../models/steemit-chat.model';
 
 
+const STEEMIT_CHAT_BASE_URL = 'https://steemit.chat';
+
+
+export interface Credentials {
+  userID:    string;
+  authToken: string;
+}
+
+
 @Injectable()
 export class SteemitChatService {
 
@@ -31,5 +40,45 @@ export class SteemitChatService {
         payload.settings = payload.settings || {};
         return <SteemitChatModel>payload;
       })
+  }
+
+  logon(username: string, password: string) : Observable<Credentials> {
+    const url = `${STEEMIT_CHAT_BASE_URL}/api/login`;
+
+    const headers = new Headers({
+      'Content-Type': 'x-www-form-urlencoded'
+    });
+
+    const body = this.encodeFormData(`user=${username}&password=${password}`);
+
+    return this.http.post(url, body, {headers})
+      .map(res => {
+        const data = res.json().data;
+        if (!data.userId) {
+          throw new Error('logon: userId not found');
+        }
+        if (!data.authToken) {
+          throw new Error('logon: authToken not found');
+        }
+        return {
+          userID:    data.userId,
+          authToken: data.authToken
+        };
+      });
+  }
+
+  logoff(creds: Credentials) : Observable<Response> {
+    const url = `${STEEMIT_CHAT_BASE_URL}/api/logoff`;
+
+    const headers = new Headers({
+      'X-User-Id':    creds.userID,
+      'X-Auth-Token': creds.authToken
+    });
+
+    return this.http.get(url, {headers});
+  }
+
+  private encodeFormData(data: string) : string {
+    return encodeURIComponent(data).replace('%20', '+');
   }
 }
